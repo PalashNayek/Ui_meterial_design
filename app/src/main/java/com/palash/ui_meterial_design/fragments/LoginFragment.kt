e8d9a21ab9ee1bb6ea38e1cab0d1b06e36d28ae6
Palash <palashnayek111@gmail.com>
2024-05-15 10:48:43 +0530

    Initial commit

---
+++ app/src/main/java/com/palash/ui_meterial_design/fragments/LoginFragment.kt
@@ -0,0 +1,39 @@
+package com.palash.ui_meterial_design.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.palash.ui_meterial_design.R
+import com.palash.ui_meterial_design.databinding.FragmentIntroBinding
+import com.palash.ui_meterial_design.databinding.FragmentLoginBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class LoginFragment : Fragment() {
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.txtRegister.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
